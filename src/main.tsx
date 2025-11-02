4a6dfc4f91e292eeab2acdf42ec0750160d7a64b
Rishav Rout <rout.rishav@gmail.com>
2025-11-01 17:15:08 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,7 @@ createRoot(document.getElementById("root")!).render(
                 />
                 <Route
                     path="/algolookup"
-                    element={<WeeklyAlgorithm appScript="https://script.google.com/macros/s/AKfycbzQShtWSjdylCinUt5Yk8YUCjzfIp1T65r4GV1-pK1vomB4UQHKiAyXyozidiZFY4OlPA/exec" />}
+                    element={<WeeklyAlgorithm appScript="https://script.google.com/macros/s/AKfycbz7xFjxGUcCmsAQxcoddHgN9TJZa6tY5hoF_ChUlrRl-ZsonzFOCFZcyoPxD6wH3CQrkA/exec" />}
                 />
             </Routes>
         </HashRouter>
