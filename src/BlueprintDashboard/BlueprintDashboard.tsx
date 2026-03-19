0df4f767a219eaee2d60223417c69216fa654141
Rishav Rout <rout.rishav@gmail.com>
2026-03-18 19:23:28 -0500

    cleaner name

--- src/BlueprintDashboard/BlueprintDashboard.tsx
+++ src/BlueprintDashboard/BlueprintDashboard.tsx
@@ -402,7 +402,7 @@ export default function BlueprintDashboard() {
 
         const link = document.createElement('a');
         link.href = dataUrl;
-        link.download = `${downloadBlueprintName}_${i}attempts_${dataUrl.length}.png`;
+        link.download = `${downloadBlueprintName}.png`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -446,7 +446,7 @@ export default function BlueprintDashboard() {
 
         const link = document.createElement('a');
         link.href = dataUrl;
-        link.download = `${downloadBlueprintName}_${i}attempts_${dataUrl.length}.png`;
+        link.download = `${downloadBlueprintName}.png`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
